6dc133a7070596854afd75f70006bd5a5edf7561
BRWEB <52031484+rockbell89@users.noreply.github.com>
2022-12-09 21:44:31 +0900

    useReducer 사용

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useRef, useCallback, useReducer } from 'react';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
 
+const COUNT = 2500;
+
 const createBulkTodos = () => {
   const array = [];
-  for (let i = 1; i <= 2500; i++) {
+  for (let i = 1; i <= COUNT; i++) {
     array.push({
       id: i,
       text: `할 일 ${i}`,
@@ -15,18 +17,35 @@ const createBulkTodos = () => {
   return array;
 };
 
-const App = () => {
-  const [todos, setTodos] = useState(createBulkTodos);
+const todoReducer = (todos, action) => {
+  switch (action.type) {
+    case 'INSERT':
+      return todos.concat(action.todo);
+    case 'REMOVE':
+      return todos.filter((todo) => todo.id !== action.id);
+    case 'TOGGLE':
+      return todos.map((todo) =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo,
+      );
+    default:
+      return todos;
+  }
+};
 
-  const nextId = useRef(4);
+const App = () => {
+  // const [todos, setTodos] = useState(createBulkTodos)
+  // 두번째 파라미터 undefined
+  // 세번째 파라미터에 초기상태를 만들어주는 함수 -> 처음 렌더링할때만 함수 호출
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
+
+  const nextId = useRef(COUNT + 1);
 
   const onToggle = useCallback((id) => {
     console.log('toggle');
-    setTodos((todos) =>
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, checked: !todo.checked } : todo,
-      ),
-    );
+    dispatch({
+      type: 'TOGGLE',
+      id,
+    });
   }, []);
 
   const onInsert = useCallback((text) => {
@@ -35,12 +54,18 @@ const App = () => {
       text,
       checked: false,
     };
-    setTodos((todos) => todos.concat(newTodo));
+    dispatch({
+      type: 'INSERT',
+      newTodo,
+    });
     nextId.current += 1;
   }, []);
 
   const onRemove = useCallback((id) => {
-    setTodos((todo) => todos.filter((todo) => todo.id !== id));
+    dispatch({
+      type: 'REMOVE',
+      id,
+    });
   }, []);
 
   return (
